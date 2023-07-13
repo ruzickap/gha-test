2b03d6c7f790b7066691091d02ea97d0ddce8ea2
Petr Ruzicka <petr.ruzicka@gmail.com>
2023-07-13 11:40:13 +0200

    feat(go): add go files + change GitHub Action

---
+++ main.go
@@ -0,0 +1,12 @@
+// Copyright © 2023 The Wizctl Authors.
+
+// Package main is the 'root' of the package hierarchy for a program.
+// This code is part of the main program, not another package,
+// and is declared as package main.
+package main
+
+import "github.com/ruzickap/wizctl/cmd"
+
+func main() {
+	cmd.Execute()
+}
