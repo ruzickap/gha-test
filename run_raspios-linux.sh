95f287ee85e246be1a3d4edc6b736e2a317b17b7
Petr Ruzicka <petr.ruzicka@gmail.com>
2023-08-29 11:46:20 +0200

    refactor(rpi): improve rpi script

--- run_raspios-linux.sh
+++ run_raspios-linux.sh
@@ -2,9 +2,9 @@
 
 set -euxo pipefail
 
-DEVICE="mmcblk0"
+DEVICE="sdb"
 
-wget -c https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-11-08/2021-10-30-raspios-bullseye-armhf-lite.zip
+wget -c https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2023-05-03/2023-05-03-raspios-bullseye-arm64-lite.img.xz
 
 cat > owner.txt << EOF
 Petr Ruzicka
@@ -18,18 +18,20 @@ lsblk --output NAME,MODEL,MODEL | grep ${DEVICE}
 
 umount /run/media/liveuser/boot /run/media/liveuser/rootfs || true
 
-# read -r -p "Press enter to remove everything from ${DEVICE} !!!"
+read -r -p "Press enter to remove everything from ${DEVICE} !!!"
 
-zcat ./*raspios*.zip | sudo dd of=/dev/${DEVICE} bs=4M conv=fsync status=progress
+xzcat ./*raspios*.xz | sudo dd of=/dev/${DEVICE} bs=4M
 sudo partprobe /dev/${DEVICE}
 
 MYTMP=$(mktemp --directory)
-sudo mount /dev/${DEVICE}p1 "${MYTMP}"
+sudo mount /dev/${DEVICE}1 "${MYTMP}"
 sudo touch "${MYTMP}/ssh"
+RPI_USER_PASSWORD=$(openssl passwd -6 raspberry)
+sudo bash -c "echo \"pi:${RPI_USER_PASSWORD}\" > \"${MYTMP}/userconf.txt\""
 sudo cp owner.txt "${MYTMP}/"
 sudo umount "${MYTMP}"
 
-sudo mount "/dev/${DEVICE}p2" "${MYTMP}"
+sudo mount "/dev/${DEVICE}2" "${MYTMP}"
 sudo bash -c "cat >> ${MYTMP}/etc/dhcpcd.conf" << EOF
 interface eth0
 static ip_address=192.168.1.2/24
@@ -38,4 +40,3 @@ static domain_name_servers=1.1.1.1
 EOF
 sudo cp owner.txt "${MYTMP}/"
 sudo umount "${MYTMP}"
-sudo sync
