1c50483753ad7950fe5512cf6d1ca85c45c2152d
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-01-29 22:05:40 +0100

    feat(gha): enable megalinter (#120)

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 set -euxo pipefail
 
-docker run --rm -v $PWD:/mnt -w /mnt ghcr.io/xu-cheng/texlive-full bash -euxc "apk add poppler-utils && make jpg pdf png svg mostlyclean"
+docker run --rm -v "${PWD}:/mnt" -w /mnt ghcr.io/xu-cheng/texlive-full bash -euxc "apk add poppler-utils && make jpg pdf png svg mostlyclean"
