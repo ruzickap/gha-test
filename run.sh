97b9598356cea75d57773a1ec606027543c48ee8
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-01-30 17:00:09 +0100

    feat(latex): remove all latex test code

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-docker run --rm -v "${PWD}:/mnt" -w /mnt ghcr.io/xu-cheng/texlive-full bash -euxc "apk add poppler-utils && make jpg pdf png svg mostlyclean"
