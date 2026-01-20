a82b3fb4a17ba5435475cc8d0789ce3267cbb731
Petr Ruzicka <petr.ruzicka@gmail.com>
2026-01-20 05:18:48 +0100

    chore: improve megalinter

---
+++ cloudwatch-log-group-unencrypted.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"
+    }
+  }
+  required_version = "~> 1.11"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Misconfiguration: CloudWatch Log Group without KMS encryption
+# trivy:ignore:AVD-AWS-0017
+resource "aws_cloudwatch_log_group" "unencrypted" {
+  #checkov:skip=CKV_AWS_158:Test resource intentionally left unencrypted
+  name              = "unencrypted-log-group"
+  retention_in_days = 365
+}
